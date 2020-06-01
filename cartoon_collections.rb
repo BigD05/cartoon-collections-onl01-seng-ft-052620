6c82ca71322b3cb131f6b93f36e67dd4d835b6a1
kyle Dornan <kyledornan26@gmail.com>
2020-06-01 17:02:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(call)
-  
-
+  calls.any? do |call|
+    call.length > 4
+  end
+end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
