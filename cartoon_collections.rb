e9c11115ea0b0f82cac6df3631b03b5cfb4259a7
kyle Dornan <kyledornan26@gmail.com>
2020-06-01 17:07:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ def long_planeteer_calls(calls)
 end
 def find_the_cheese(array)
 cheese_types = ["cheddar", "gouda", "camembert"]
-cheese_types.include?(cheddar)
+cheese_types.include?("cheddar")
 end
