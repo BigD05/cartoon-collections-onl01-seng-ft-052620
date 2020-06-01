1893b8828374ede47c6abaa6809ff68fe8d2aea7
kyle Dornan <kyledornan26@gmail.com>
2020-06-01 17:02:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,11 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(call)
-  calls.any? do |call|
-    call.length > 4
-    true 
-end
-end
+  
 
 def find_the_cheese# code an argument here
   # the array below is here to help
